8b364e1b25fa87a426d93e9b21e973205a2b48dc
XxXM4RI0XxX <montejanodiaza@gmail.com>
2024-08-29 21:54:21 -0600

    Curso C Yutub

---
+++ Ejercicio_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+    /*
+    Realice un programa que determine cual es la nota final de un alumno en una materia teniendo en cuenta que la nota final es el promedio de 2 examenes. (obtener el promedio de los 2 examenes)
+    */
+int main(){
+
+    float ex1 , ex2;
+
+    printf("Ingrese nota primer examen: ");
+    scanf("%f",&ex1);
+    printf("Ingrese nota segundo examen: ");
+    scanf("%f",&ex2);
+
+    float prom = (ex1+ex2)/2;
+
+    printf("%f es el promedio final",prom);
+
+}
--- welcome.c
+++ welcome.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
+//Definir constantes
+#define SEMESTRE 3.5;
+#define NOMBRE "Jose";
+//Definir variables
+float dinero = 127.33;
+char pollo = 'P';
 
-int main(){
+void main(){
 
-    printf("Welcome\n");
-    printf("Boli :3");
+    int ola = 14; //Declarar variable
+    float adios = 15;
+    double sum = ola-adios;
+    printf("%d\n",ola);
+    ola+=adios;
+
+    printf("Welcome\n");//Imprimir texto en consola
+    printf("%d - %f = %f\n",ola,adios,sum);
     //comment xd
-    return 0;
+   
     /*
     comment block
     */
 
+   //Secuencias de escape
+   printf("Nu\n\0lo olo87wdytf789\n");//Canela todo el texto que haya despues de Nulo
+   printf("Retro\bceso\n");//Aplica un... retroceso en el texto?
+   printf("Tabu\tlador\n");//Tabula xd
+   printf("Salto de>\n<linea\n");//Salta la linea :o
+   printf("Salto de \f pagina\n");// ??
+   printf("Retorno de\r carro carro ca\rrro?\n");// FF
+   printf("Poner \"comillas\" | Poner ¿Signo de interrogacion?\? | Poner \\ barra \\ invertida\n");//Para mostrar signos reservados (" \ ?)
 }
